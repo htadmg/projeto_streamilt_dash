b0677597fa450b8009c1b1d3be98d6584d97f9e4
htadmg <agatadominguestec@hotmail.com>
2024-09-11 15:54:18 -0300

    commit inicial

---
+++ utils.py
@@ -0,0 +1,40 @@
+
+from dataset import df
+import pandas as pd
+import streamlit as st
+import time
+
+def format_number(value, prefix=''):
+    for unit in ['', 'mil']:
+        if value < 1000:
+            return f'{prefix} {value:.2f} {unit}'
+        value /= 1000
+
+    return f'{prefix} {value:.2f} milhões'
+
+
+# 1- DataFrame Receita por estado
+df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
+df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(df_rec_estado, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
+
+# 2- DataFrame Receita mensal
+df_rec_mensal = df.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+
+df_rec_mensal['Ano'] = df_rec_mensal['Data da Compra'].dt.year
+df_rec_mensal['Mes'] = df_rec_mensal['Data da Compra'].dt.month_name()
+
+# 3- DataFrame Receita por Categoria
+df_rec_categoria = df.groupby('Categoria do Produto')[['Preço']].sum().sort_values('Preço', ascending=False)
+
+# 4-DataFrame Vendedores
+df_vendedores = pd.DataFrame(df.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
+
+# função para converter arquivo csv
+@st.cache_data
+def convert_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+def mensagem_sucesso():
+    sucess = st.success('Arquivo baixado com sucesso')
+    time.sleep(3)
+    sucess.empty()
